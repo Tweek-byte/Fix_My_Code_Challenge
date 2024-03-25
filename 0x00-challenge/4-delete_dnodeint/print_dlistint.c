c06fc7aab59c4647a395a302bd4bbf3de6be8bf6
Tweek-Byte <zaka4396@gmail.com>
2024-03-24 17:51:04 -0700

    task 5

---
+++ 0x00-challenge/4-delete_dnodeint/print_dlistint.c
@@ -0,0 +1,26 @@
+97% of storage used … 
+If you run out, you can't create, edit and upload files. Get 100 GB of storage for MAD 19.00 MAD 0 for 1 month.
+
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints a doubly linkedlist of integers
+ *
+ * @h: A pointer to the first element of a list
+ *
+ * Return: The number of element printed
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
